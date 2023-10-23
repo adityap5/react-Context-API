a4df9202194b384bf72a8f4721d409cd813435ba
adityap5 <aditya29pippal@gmail.com>
2023-10-23 22:39:59 +0530

    react-toastify

--- app/page.js
+++ app/page.js
@@ -1,17 +1,33 @@
 "use client"
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import React, { useContext } from 'react'
 import { MyContext } from './Helper/Context'
 import Nav from './Component/Nav'
 
 const page = () => {
-
+const notify = ()=>{
+  toast.success('Login Done!!', {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    });
+ 
+}
   const user =  useContext(MyContext)
   return (
     <>
     <Nav/>
-    <h1>HELLO!!!!{user}</h1>
-
+    <div>
+    <button onClick={notify}>Login</button>
+   <ToastContainer/>
+    </div>
+ 
     </>
     )
 }
